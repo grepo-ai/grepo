6405e9ebe1e1191154fab226c6e4693e9b1dc9eb
tausif <mtausif_0003@yahoo.com>
2025-08-18 04:20:01 +0530

    Added file edit-in-place utility function (optimised version), tested with agent works fine but need more tests

--- src/agent/utils.py
+++ src/agent/utils.py
@@ -1,6 +1,11 @@
 import uuid
 import sqlite3
 import re
+import os
+import tempfile
+import shutil
+
+
 from langgraph.checkpoint.sqlite import SqliteSaver
 import diff_match_patch as dmp_module
 
@@ -20,9 +25,58 @@ def generate_session_uuid():
 def apply_diff(file_path, old_code, new_code):
     "Applies the code diff to the file"
 
-    with open(file_path, "rb+") as file:
-        file.write(generated_code.encode("utf-8"))
-    return
+    old_code = old_code.strip()
+    new_code = new_code.strip()
+
+    # Get the diffs
+    diffs, patches = generate_diff(old_code, new_code, file_path)
+
+    # Apply diff to old code
+    dmp = dmp_module.diff_match_patch()
+    patched_code, _ = dmp.patch_apply(patches, old_code)
+
+    # Write the code to new file and replace with old file
+    patched_code_lines = patched_code.splitlines()
+    old_code_lines = old_code.splitlines()
+
+    write_new_line_count = 0
+    skip_lines = 0
+    with (
+        open(file_path, "r") as src_file,
+        tempfile.NamedTemporaryFile(
+            "w", delete=False, dir=os.path.dirname(file_path), encoding="utf-8"
+        ) as tmp_file,
+    ):
+        tmp_name = tmp_file.name
+
+        for line_no, line in enumerate(src_file, start=0):
+            new_line = "\n" if line.endswith("\n") else ""
+            stripped_line = line[:-1] if new_line else line
+
+            if stripped_line == old_code_lines[0]:
+                while write_new_line_count < len(patched_code_lines):
+                    tmp_file.write(f"{patched_code_lines[write_new_line_count]}\n")
+                    write_new_line_count += 1
+
+                # Skip lines for maximum length b/w old_code and patched code
+                # as we have already written the patched code so we need to skip lines from exisitng file
+                # to avoid inconsistency in data
+                skip_lines = max(len(old_code_lines), write_new_line_count) + line_no
+
+            elif skip_lines and line_no < skip_lines:
+                continue
+
+            else:
+                tmp_file.write(stripped_line + new_line)
+
+        if write_new_line_count > 0:
+            # Atomic replace operation and safe
+            shutil.copystat(file_path, tmp_name, follow_symlinks=False)
+            os.replace(tmp_name, file_path)
+        else:
+            os.unlink(tmp_name)
+
+    return True if write_new_line_count > 0 else False
 
 
 def generate_diff(old_code, new_code, file_path, highlight=False):
@@ -37,7 +91,8 @@ def generate_diff(old_code, new_code, file_path, highlight=False):
     dmp.diff_cleanupEfficiency(diffs)
 
     if not highlight:
-        return diffs
+        patches = dmp.patch_make(old_code, diffs)
+        return diffs, patches
 
     # Rich highlight the diff
     rich_old_text = ""

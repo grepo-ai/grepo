e999467301d4b4c9e95f1a5809093dc1bce04b7a
tausif <mtausif_0003@yahoo.com>
2025-09-03 01:05:38 +0530

    init glob tool

---
+++ src/agent/tools/glob_tool.py
@@ -0,0 +1,18 @@
+import os
+import glob as std_glob
+import re
+from typing import Annotated, Optional
+from typing_extensions import TypedDict
+
+from langchain_core.tools import tool, InjectedToolCallId
+from langchain_core.messages import ToolMessage
+from langgraph.prebuilt import InjectedState
+
+from src.agent.state import GlobalState
+from langgraph.types import Command, interrupt
+from agent.utils import apply_diff, generate_diff
+
+
+@tool
+def glob():
+    pass
